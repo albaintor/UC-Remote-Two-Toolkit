0583082682a1de444a3a632f2e64cebf704579bb
albaintor <118518828+albaintor@users.noreply.github.com>
2024-04-28 09:07:24 +0200

    Fixed server mount point

--- server/app.js
+++ server/app.js
@@ -35,7 +35,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static('public/browser'))
+app.use('/', express.static('public/browser'))
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
